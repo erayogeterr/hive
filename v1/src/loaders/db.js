fb22e539ca341de6ced792367952259fa0cd1096
Eray Ögeter <erayogeterr@gmail.com>
2023-04-04 14:05:36 +0300

    render deployment#1

--- v1/src/loaders/db.js
+++ v1/src/loaders/db.js
@@ -2,33 +2,27 @@ const Mongoose = require ("mongoose");
 const db = Mongoose.connection;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://erayogeterr:hiveproject676767@hive.stkuqcg.mongodb.net/?retryWrites=true&w=majority";
+
+
 db.once("open", () => {
     console.log("DB Bağlantısı başarılıdır.")
 })
 
+//LocalHost
 const connectDB = async () => {
-   //await Mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-     
-   //await Mongoose.connect(`mongodb+srv://erayogeterr:<0123107038>@hive.stkuqcg.mongodb.net/?retryWrites=true&w=majority`,
-    //MongoClient.connect(`mongodb+srv://erayogeterr:hiveproject676767@hive.stkuqcg.mongodb.net/?retryWrites=true&w=majority`,{
-      //  useUnifiedTopology: true })
-        //.then(client => {
-          //  console.log(`connected to Database`)
-           // const db = client.db(`hive`)
-       // });
-   // };
-   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  // await Mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+   // useNewUrlParser: true,
+   // useUnifiedTopology: true,
+   //});
+}; 
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("hive").collection("hosts");
+  const collection = client.db("hive")
   // perform actions on the collection object
   client.close();
 });
-   
-}
-  // {
-    //    useNewUrlParser:true,
-      //  useUnifiedTopology: true,
-    //};
+
 module.exports = {
     connectDB,
 }
