5228f25233a3473eb66939b0baef4dafcf0e5dcb
Eray Ögeter <erayogeterr@gmail.com>
2023-04-04 20:33:15 +0300

    render deploy.

--- v1/src/loaders/db.js
+++ v1/src/loaders/db.js
@@ -1,28 +1,21 @@
-const Mongoose = require ("mongoose");
-const db = Mongoose.connection;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://erayogeterr:hiveproject676767@hive.stkuqcg.mongodb.net/?retryWrites=true&w=majority";
+const Mongoose = require("mongoose");
 
+const db = Mongoose.connection;
 
 db.once("open", () => {
-    console.log("DB Bağlantısı başarılıdır.")
-})
-
-//LocalHost
-const connectDB = async () => {
-    // await Mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    //  });
-}; 
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-const collection = client.db("hive")
-client.close();
+  console.log("DB bağlandı");
 });
 
+const connectDB = async () => {
+  await Mongoose.connect(
+       process.env.DATABASE_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+};
 
 module.exports = {
-    connectDB,
-}
+  connectDB,
+};
