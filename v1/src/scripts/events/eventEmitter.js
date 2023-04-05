d5c1b45dbd72929fc0647c13e719ddab924eaf25
Eray Ögeter <erayogeterr@gmail.com>
2023-04-05 11:13:37 +0300

    updated mailer.

--- v1/src/scripts/events/eventEmitter.js
+++ v1/src/scripts/events/eventEmitter.js
@@ -1,2 +1,7 @@
-const events = require("events")
-module.exports = new events.EventEmitter();
+// const events = require("events")
+// module.exports = new events.EventEmitter();
+
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+module.exports = eventEmitter;
