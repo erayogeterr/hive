ef978bafd5db65648fdcc8d9354410114fe75a63
Eray Ögeter <erayogeterr@gmail.com>
2023-04-07 00:36:44 +0300

    some changes.

--- v1/src/middlewares/authenticate.js
+++ v1/src/middlewares/authenticate.js
@@ -10,9 +10,9 @@ const authenticateToken = (req,res,next) => {
    }
    
 
-   JWT.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, host) => {
+   JWT.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, user) => {
     if(err) return res.status(httpStatus.FORBIDDEN).send({ error : err })
-    req.host = host?.doc;
+    req.user = user?.doc;
     next();
    })
 }
