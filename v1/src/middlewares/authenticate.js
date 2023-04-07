620a183a5c523a488d5301a278a88ff0f413d43f
Eray Ögeter <erayogeterr@gmail.com>
2023-04-07 11:42:12 +0300

    some changes.

--- v1/src/middlewares/authenticate.js
+++ v1/src/middlewares/authenticate.js
@@ -2,19 +2,34 @@ const httpStatus = require("http-status")
 const JWT = require("jsonwebtoken")
 
 
-const authenticateToken = (req,res,next) => {
-   const authHeader =  req.headers["authorization"]
-   const token = authHeader && authHeader.split(" ")[1]
-   if(token == null) {
-    return res.status(httpStatus.UNAUTHORIZED).send({ error : "Bu işlemi yapmak için ilk olarak giriş yapmalısınız."})
-   }
+// const authenticateToken = (req,res,next) => {
+//    const authHeader =  req.headers["authorization"]
+//    const token = authHeader && authHeader.split(" ")[1]
+//    if(token == null) {
+//     return res.status(httpStatus.UNAUTHORIZED).send({ error : "Bu işlemi yapmak için ilk olarak giriş yapmalısınız."})
+//    }
    
 
+//    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, user) => {
+//     if(err) return res.status(httpStatus.FORBIDDEN).send({ error : err })
+//     req.user = user?.doc;
+//     next();
+//    })
+// }
+
+const authenticateToken = (req, res, next) => {
+   const authHeader = req.headers["authorization"];
+   if (!authHeader) {
+     return res.status(httpStatus.UNAUTHORIZED).send({ error: "Bu işlemi yapmak için ilk olarak giriş yapmalısınız."});
+   }
+   const token = authHeader.split(" ")[1];
    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, user) => {
-    if(err) return res.status(httpStatus.FORBIDDEN).send({ error : err })
-    req.user = user?.doc;
-    next();
-   })
-}
+     if (err) {
+       return res.status(httpStatus.FORBIDDEN).send({ error: err.message });
+     }
+     req.user = user.doc;
+     next();
+   });
+ };
 
 module.exports = authenticateToken;
