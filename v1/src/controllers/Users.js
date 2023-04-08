2a6d6e48f82ea8be3915058f3d630d87f63d09d3
Eray Ögeter <erayogeterr@gmail.com>
2023-04-08 16:49:00 +0300

    some changes..

--- v1/src/controllers/Users.js
+++ v1/src/controllers/Users.js
@@ -62,7 +62,7 @@ const resetPassword = (req,res) => {
 
 const update = (req, res) => {
     //modify({ _id : req.user?._id }, req.body.user._id)
-    modify({_id:req.body.user._id},req.body)
+    modify({_id:req.body.user},req.body)
         .then((updatedUser) => {
             res.status(httpStatus.OK).send(updatedUser);
         })
