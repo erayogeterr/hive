610765b71cd7f2b12be3a2243bd6dc4052686dc8
Eray Ögeter <erayogeterr@gmail.com>
2023-04-08 12:18:04 +0300

    User is included in the system.

--- v1/src/controllers/Users.js
+++ v1/src/controllers/Users.js
@@ -61,7 +61,7 @@ const resetPassword = (req,res) => {
 };
 
 const update = (req, res) => {
-    modify({ _id : req.user?._id }, req.body)
+    modify({ id : req.user?._id }, req.body)
         .then((updatedUser) => {
             res.status(httpStatus.OK).send(updatedUser);
         })
