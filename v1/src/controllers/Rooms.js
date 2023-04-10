2e2b32ad08ee4d71b870a338bcde33b253b4f8ca
Eray Ögeter <erayogeterr@gmail.com>
2023-04-10 15:19:44 +0300

    Added room and participants.Crud and JoinRoom operations are complated.

--- v1/src/controllers/Rooms.js
+++ v1/src/controllers/Rooms.js
@@ -5,7 +5,7 @@ const Rooms = require("../models/Rooms");
 const Participant = require('../models/Participants');
 
 const create = (req, res) => {
-    //req.body.password = passwordToHash(req.body.password);
+    req.body.password = passwordToHash(req.body.password);
     insert(req.body)
         .then((response) => {
             res.status(httpStatus.CREATED).send(response);
@@ -37,51 +37,46 @@ const getByIdRoom = (req, res) => {
 
 const deleteRoom = (req, res) => {
     remove(req.params.id)
-    .then((deletedItem) => {
-        if (!deletedItem) {
-            return res.status(httpStatus.NOT_FOUND).send({message: "Böyle bir oda bulunmamaktadır.",});
-        }
-        res.status(httpStatus.OK).send({message: "Oda silinmiştir.",
-        });
-    })
-    .catch((e) => res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error: "Silme işlemi sırasında hata ile karşılaşıldı." }));
+        .then((deletedItem) => {
+            if (!deletedItem) {
+                return res.status(httpStatus.NOT_FOUND).send({ message: "Böyle bir oda bulunmamaktadır.", });
+            }
+            res.status(httpStatus.OK).send({
+                message: "Oda silinmiştir.",
+            });
+        })
+        .catch((e) => res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error: "Silme işlemi sırasında hata ile karşılaşıldı." }));
 }
 
 const JoinRoom = async (req, res) => {
     try {
         const roomId = req.params.id // roomId'yi parametre olarak al
-        console.log("roomId : ",roomId);
-        const password = req.body.password; // password'u istek gövdesinden al
-        console.log("req body password : ", password);
-        const anonymousName = "anonymous-" + Math.random().toString(36).substring(7);
-        
+        const password = passwordToHash(req.body.password); // password'u istek gövdesinden al
+
         // Oda ID'sini kullanarak odayı bul
         const room = await Rooms.findById(roomId);
-        console.log("room password: " + room.password);
-        if (!room) {
-          return res.status(404).json({ message: 'Oda bulunamadı' });
-        }
-        
+
         // Şifreyi kontrol et
         if (room.password !== password) {
-          return res.status(401).json({ message: 'Geçersiz şifre' });
+            return res.status(httpStatus.UNAUTHORIZED).send({ message: 'Geçersiz şifre' });
         }
-        
+
         // Yeni bir katılımcı oluştur ve odaya ekle
-        const participant = new Participant({ name : anonymousName ,room: room.id });
+        const participant = new Participant({room: room.id});
         await participant.save();
-        console.log(participant.name);
-        room.participants.push(participant.id);
+
+        let participants = room.participants || [];
+        participants.push(participant.name);
+        room.participants = participants;
+
         await room.save();
-    
-        return res.status(201).json({ message: 'Katılımcı başarıyla eklendi' });
-      } catch (err) {
+
+        return res.status(httpStatus.CREATED).send({ message: 'Katılımcı başarıyla eklendi' });
+    } catch (err) {
         console.error(err);
-        return res.status(500).json({ message: 'Sunucu hatası' });
-      }
-    };
-
-
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ message: 'Sunucu hatası' });
+    }
+};
 
 
 module.exports = {
--- v1/src/models/Participants.js
+++ v1/src/models/Participants.js
@@ -1,18 +1,15 @@
-  const Mongoose = require("mongoose")
+   const Mongoose = require("mongoose")
 
-  const ParticipantSchema = new Mongoose.Schema({
-      id : {
-        type : Mongoose.Schema.Types.ObjectId,
-      },
-      name: {
-          type: String,
-          //default: () => "anonymous-" + Math.random().toString(36).substring(7),
-        },
-        room: {
-          type: Mongoose.Schema.Types.ObjectId,
-          ref: "Rooms"
-        }
-      }, { timestamps: true, versionKey: false });
+   const ParticipantSchema = new Mongoose.Schema({
+       name: {
+           type: String,
+           default: () => "anonymous-" + Math.random().toString(36).substring(7),
+         },
+         room: {
+           type: Mongoose.Schema.Types.ObjectId,
+           ref: "Rooms"
+         }
+       }, { timestamps: true, versionKey: false });
 
-  module.exports = Mongoose.model("participant", ParticipantSchema);
+   module.exports = Mongoose.model("participant", ParticipantSchema);
 
