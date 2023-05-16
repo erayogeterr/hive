632b621d29be001bf3134a8487703dd87336632c
Eray Ögeter <erayogeterr@gmail.com>
2023-05-16 22:56:20 +0300

    partipicant ip.

--- v1/src/controllers/Rooms.js
+++ v1/src/controllers/Rooms.js
@@ -259,6 +259,7 @@ const JoinRoom = async (req, res) => {
    // const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
    //const clientIp = req.headers['x-real-ip'] || req.socket.remoteAddress;
    const clientIp = req.ip
+   console.log(clientIp);
     const existingParticipant = await Participant.findOne({ room: room.id, ip: clientIp });
 
     if (existingParticipant) {
