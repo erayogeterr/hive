2cf884a69be730810f24b28ec4ec070c8d65dde4
Eray Ögeter <erayogeterr@gmail.com>
2023-05-22 15:08:38 +0300

    socket.

--- v1/src/loaders/db.js
+++ v1/src/loaders/db.js
@@ -14,18 +14,18 @@ db.once("open", () => {
               //      });
               //   };
 
-                 const connectDB = async () => {
-                 await Mongoose.connect(
-                    process.env.DATABASE_URL, 
-// // // // // // // // // // // // // // // // //       process.env.ENV === "dev"
-// // // // // // // // // // // // // // // // //      //   ? `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-// // // // // // // // // // // // // // // // //      //   : process.env.DB_LINK,
-                   {
-                    useNewUrlParser: true,
-                      useUnifiedTopology: true,
-                     }
-                   );
-                };
+                  const connectDB = async () => {
+                  await Mongoose.connect(
+                     process.env.DATABASE_URL, 
+// // // // // // // // // // // // // // // // // //       process.env.ENV === "dev"
+// // // // // // // // // // // // // // // // // //      //   ? `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// // // // // // // // // // // // // // // // // //      //   : process.env.DB_LINK,
+                    {
+                     useNewUrlParser: true,
+                       useUnifiedTopology: true,
+                      }
+                    );
+                 };
 
 module.exports = {
   connectDB,
