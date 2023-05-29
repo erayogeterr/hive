80283b370e88201a129b604320b94abb7d5d0de7
Eray Ögeter <erayogeterr@gmail.com>
2023-05-29 19:58:49 +0300

    like question.

--- v1/src/app.js
+++ v1/src/app.js
@@ -1,38 +1,3 @@
-// const express = require("express");
-// const fileUpload = require("express-fileupload");
-// const helmet = require("helmet")
-// const config = require("./config");
-// const loaders = require("./loaders");
-// const events = require("./scripts/events");
-// const { UserRoutes, RoomRoutes } = require("./api-routes");
-// const cors = require("cors")
-// const path = require("path");
-// const http = require('http');
-// const socketIO = require('socket.io');
-// const server = http.createServer(app);
-// const io = socketIO(server);
-
-// require('./QuestionController')(io);
-// config();
-// loaders();
-
-// const app = express();
-// app.use(express.json());
-// app.use(helmet());
-// app.use(cors());
-// app.use(fileUpload());
-// app.use("/uploads", express.static(path.join(__dirname, "./", "uploads")));
-// app.set('trust proxy', true);
-
-
-
-
-
-// app.listen(process.env.APP_PORT || 8000, () => {
-//     console.log("Sunucu ayağa kalktı.");
-//     app.use("/users", UserRoutes);
-//     app.use("/rooms", RoomRoutes);
-// })
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const helmet = require("helmet");
@@ -41,11 +6,8 @@ const loaders = require("./loaders");
 const events = require("./scripts/events");
 const { UserRoutes, RoomRoutes, QuestionRoutes, PartipicantRoutes } = require("./api-routes");
 const cors = require("cors");
-
 const path = require("path");
-
 const http = require('http');
-
 const socketIO = require('socket.io');
 const app = express();
 const server = http.createServer(app);
@@ -61,10 +23,8 @@ config();
 loaders();
 const { questionSocket } = require('./controllers/QuestionController');
 const { partipicantSocket } = require('./controllers/ParticipantController');
-//const { JoinRoom } = require('./controllers/Rooms');
 questionSocket(io);
 partipicantSocket(io);
-//JoinRoom(io);
 
 app.use(express.json());
 app.use(helmet());
